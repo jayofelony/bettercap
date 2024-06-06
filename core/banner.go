6e5ac735eb26e7ccbc0e0e94b1bb501e8d78fa29
jayofelony <oudshoorn.jeroen@gmail.com>
2024-06-06 09:00:10 +0200

    dev
    Signed-off-by: jayofelony <oudshoorn.jeroen@gmail.com>

--- core/banner.go
+++ core/banner.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "bettercap"
-	Version = "2.32.4"
+	Version = "2.32.5"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Editor  = "Jeroen 'Jayofelony' Oudshoorn"
 	Website = "https://bettercap.org/"
