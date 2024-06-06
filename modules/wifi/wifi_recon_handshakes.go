27f2bf4b8b6cb92c6a1e780f58c445ca657d2499
jayofelony <oudshoorn.jeroen@gmail.com>
2024-06-06 09:08:28 +0200

    dev
    Signed-off-by: jayofelony <oudshoorn.jeroen@gmail.com>

--- modules/wifi/wifi_recon_handshakes.go
+++ modules/wifi/wifi_recon_handshakes.go
@@ -99,7 +99,7 @@ func (mod *WiFiModule) discoverHandshakes(radiotap *layers.RadioTap, dot11 *laye
 		numUnsaved := station.Handshake.NumUnsaved()
 		shakesFileName := mod.shakesFile
 		if mod.shakesAggregate == false {
-			shakesFileName = path.Join(shakesFileName, fmt.Sprintf("%s.pcapng", ap.PathFriendlyName()))
+			shakesFileName = path.Join(shakesFileName, fmt.Sprintf("%s.pcap", ap.PathFriendlyName()))
 		}
 		doSave := numUnsaved > 0
 		if doSave && shakesFileName != "" {
